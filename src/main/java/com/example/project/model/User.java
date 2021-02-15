a660fa18b1c6b0537997077e3c18ada192ef4605
alnero <darestream@gmail.com>
2021-02-15 16:58:49 +0300

    Add JoinTable annotation

--- src/main/java/com/example/project/model/User.java
+++ src/main/java/com/example/project/model/User.java
@@ -27,6 +27,11 @@ public class User implements UserDetails {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id",referencedColumnName = "id")
+    )
     private Set<Role> roles;
 
     public User() {
