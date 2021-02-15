b514baa03d451da2035e5dcbe6439cb92b70ad55
alnero <darestream@gmail.com>
2021-02-15 17:00:21 +0300

    Add throw UsernameNotFoundException as per method contract

--- src/main/java/com/example/project/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/project/service/UserDetailsServiceImpl.java
@@ -20,6 +20,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-        return userRepository.getUserByName(name);
+        UserDetails userDetails = userRepository.getUserByName(name);
+        if (userDetails == null) {
+            throw new UsernameNotFoundException("User not found with name: " + name);
+        }
+        return userDetails;
     }
 }
